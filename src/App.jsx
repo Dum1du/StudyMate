41180a529a52ffe65159d2545fdad5a85f7aa6e0
AsithaWijerathne <wijerathneasitha@gmail.com>
2025-09-24 17:25:30 +0530

    navigationBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "./NavigationBar";
+import "./index.css";
+
+import NavigationBar from "./NavigationBar";
 
 function App() {
   return (
-    <div>
+    <>
+      <NavigationBar />
       <h1>Main Page</h1>
-    </div>
+    </>
   );
 }
 
