4c5ab4094e6635529e1f0da73bb935e5b7f8d45b
AsithaWijerathne <wijerathneasitha@gmail.com>
2025-09-24 17:28:52 +0530

    Revert "navigationBar"
    This reverts commit 41180a529a52ffe65159d2545fdad5a85f7aa6e0.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import "./NavigationBar";
-import "./index.css";
-
-import NavigationBar from "./NavigationBar";
 
 function App() {
   return (
-    <>
-      <NavigationBar />
+    <div>
       <h1>Main Page</h1>
-    </>
+    </div>
   );
 }
 
