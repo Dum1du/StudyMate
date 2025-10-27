80df22277be67e3c4a0a83b10a877f120a129d4a
AsithaWijerathne <wijerathneasitha@gmail.com>
2025-10-27 19:33:43 +0530

    profile card

--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -1,19 +1,32 @@
 import Navbar from "../NavigationBar";
+import { IoCameraOutline } from "react-icons/io5";
+
 function UserProfile() {
   return (
     <>
       <Navbar />
-      <div className="flex justify-center w-full h-50 mt-25">
-        <div className="shadow-2xl w-250 h-50 border-s-black rounded-2xs items-center">
-          <div className="justify-items-left h-full mt-8 ml-10 border-blue-500">
-            <img
-              src="https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-
+      <div className="max-w-3xl mx-auto bg-white border border-gray-200 rounded-xl shadow-sm p-6 flex items-center space-x-6 mt-25">
+        <div className="relative">
+          <img
+            src="https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-
               person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-
               templatex9xa_719432-2190.jpg?semt=ais_hybrid&w=740&q=80"
-              alt="User"
-              className="w-40 h-40 rounded-full border-2 border-white"
-            />
+            alt="User"
+            className="w-30 h-30 rounded-full object-cover border-0"
+          />
+          <div className="absolute bottom-0 right-0 rounded-full p-2 bg-white border-2 border-white cursor-pointer hover:bg-blue-600">
+            <IoCameraOutline className="text-blue-500 text-sm" />
           </div>
+        </div>
+        <div className="flex-1">
+          <h2 className="text-xl font-semibold text-gray-900">
+            Nadeesha Perera
+          </h2>
+          <p className="text-gray-600">Faculty of Engineering</p>
+          <p className="text-gray-500 text-sm">Joined 2021</p>
+          <button className="bg-blue-600 hover:cursor-pointer text-white px-4 py-1 mt-2 rounded-md font-small">
+            Edit Profile
+          </button>
         </div>
       </div>
     </>
